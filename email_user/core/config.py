403da43f6d5f37c2baecc0c1975480da5c3a16a4
Tahzib Mashrik <mashrikt@gmail.com>
2018-03-26 02:30:49 +0600

    User Model

---
+++ email_user/core/config.py
@@ -0,0 +1,10 @@
+class GenderType(object):
+    MALE = 0
+    FEMALE = 1
+    OTHERS = 2
+
+    CHOICES = (
+        (MALE, "Male"),
+        (FEMALE, "Female"),
+        (OTHERS, "Others")
+    )
--- email_user/settings.py
+++ email_user/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'email_user.core'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'core.User'
+ACCOUNT_USER_MODEL_USERNAME_FIELD = 'email'
